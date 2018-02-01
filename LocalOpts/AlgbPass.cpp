6ae135edabc97edd2ea57998eadca1fbaef43cc7
Fazil Zuhaib <zuhaibfazil@gmail.com>
2018-02-01 07:55:36 -0500

    test files for part 2 of the question, init file for algbpass

---
+++ LocalOpts/AlgbPass.cpp
@@ -0,0 +1,84 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Casting.h"
+#include <map>
+#include <string.h>
+
+using namespace llvm;
+
+namespace {
+class AlgbPass : public ModulePass
+{
+private:
+
+    std::map<std::string, int> ins_count;
+    std::map<std::string, int> calls_count;
+    bool runOnBasicBlock(BasicBlock & BB)
+    {
+        // loop through every instruction and check if its a call
+        // if it is then increment the correct datastructure.
+        for(BasicBlock::Iterator iter = BB.begin(); iter != BB.end(); ++iter)
+        {
+            if(auto *call = dyn_cast<CallInst>(*iter))
+            {
+                std::string name = call->getCalledFunction()->getName();
+                calls_count[name] += 1;
+            }
+        }
+        return false;
+    }
+
+    bool runOnFunction(Function & F)
+    {
+        std::string name = F.getName();
+        ins_count[name] = 0;
+        calls_count[name] = 0;
+        for(BasicBlock &bb : F)
+        {
+            // bb.size() == # of instructions in a basic block
+            ins_count[name] += bb.size();
+            runOnBasicBlock(bb);
+        }
+        // F.size() == # of instructions in a Function
+        outs() << name << " "<< F.arg_size() << " " << calls_count[name] << " "
+               << F.size() << " " << ins_count[name] << '\n';
+        return false;
+    }
+
+public:
+    static char ID;
+
+    AlgbPass() : ModulePass(ID)
+    {}
+
+    ~AlgbPass()
+    {}
+
+    // We don't modify the program, so we preserve all analysis.
+    virtual void getAnalysisUsage(AnalysisUsage & AU) const
+    {
+        AU.setPreservesAll();
+    }
+
+
+    virtual bool runOnModule(Module & M)
+    {
+        outs() << "Name #Args #Calls #Blocks #Insts" << "\n";
+
+        for (Module::iterator iter = M.begin(); iter != M.end(); ++iter)
+        {
+            calls_count[(*iter).getName()] = 0;
+            ins_count[(*iter).getName()] = 0;
+            runOnFunction(*iter);
+        }
+
+        // @TODO Please implement this.
+
+        return false;
+    }
+};
+
+char FunctionInfo::ID = 0; RegisterPass < FunctionInfo > X ("function-info", "CSCD70: Functions Information"); /* annoymous */ }
---
+++ LocalOpts/tests/tests.c
@@ -0,0 +1,36 @@
+int algebraic (int a, int b)
+{
+    int result = (a/a);
+
+    result *= (b/b);
+    result += (b-b);
+    result /= result;
+    result -= result;
+    return result;
+}
+
+int strength (int a, int b)
+{
+    int result = 0;
+    result += a * 2;
+    result += a * 3;
+    result += a * 8;
+    result -= b / 2;
+    result -= b / 4;
+    result -= b / 8;
+    return result;
+}
+
+int multi_inst (int b)
+{
+    int result = 0;
+    int a = b + 1;
+    int c = 4 + a - 1;
+
+    result += a;
+    result += b;
+    result *= c;
+    result /= 2;
+    return result;
+}
+
