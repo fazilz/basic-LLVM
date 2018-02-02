679eb763f6e6562f1fbc7925d49dab2ab7f7d2bf
Fazil Zuhaib <zuhaibfazil@gmail.com>
2018-02-01 20:36:43 -0500

    including constantsint

--- LocalOpts/LocalOpts.cpp
+++ LocalOpts/LocalOpts.cpp
@@ -3,6 +3,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/ADT/APInt.h"
